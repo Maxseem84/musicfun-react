6f4ee7489b720c2ee72b0893c703ca5c0784fb1c
Maxseem84 <maksim.p.gorbenko@gmail.com>
2026-01-11 13:32:11 +0200

    env api-key

--- src/dal/api.ts
+++ src/dal/api.ts
@@ -10,13 +10,17 @@ export type GetTrackDetailsOutput = {
   data: GetTrackDetailsOutputData;
 };
 
+const prepareHeaders = () => {
+  const apiKey = import.meta.env.VITE_API_KEY;
+  if (!apiKey) return undefined;
+  return { 'api-key': apiKey };
+};
+
 export const getTrack = (trackId: string) => {
   const promise: Promise<GetTrackDetailsOutput> = fetch(
     'https://musicfun.it-incubator.app/api/1.0/playlists/tracks/' + trackId,
     {
-      headers: {
-        'api-key': '97802113-ea06-4980-96da-ce77494e42fd',
-      },
+      headers: prepareHeaders(),
     },
   ).then((res) => res.json());
   return promise;
@@ -44,9 +48,7 @@ export const getTracks = () => {
   const promise: Promise<GetTrackListOutput> = fetch(
     'https://musicfun.it-incubator.app/api/1.0/playlists/tracks',
     {
-      headers: {
-        'api-key': '97802113-ea06-4980-96da-ce77494e42fd',
-      },
+      headers: prepareHeaders(),
     },
   ).then((res) => res.json());
   return promise;
